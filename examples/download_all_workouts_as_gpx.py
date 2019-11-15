f4260494fd3869396b8134c0ba6cee73056c944e
filip_trojan <Tarantula2018>
2019-11-15 22:13:33 +0100

    work in progress

--- examples/download_all_workouts_as_gpx.py
+++ examples/download_all_workouts_as_gpx.py
@@ -1,10 +1,24 @@
+import logging
+import yaml
 from sports_tracker_api.sports_tracker import SportsTracker
 
+logging.basicConfig(
+    level=logging.INFO,
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    format="%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),
+    ],
+)
+
 out_dir = "data"
+with open("data/secrets.yaml") as file:
+    secrets = yaml.load(file)
 tracker = SportsTracker(
-    username="ftrojan",
-    password="",
+    username=secrets['username'],
+    password=secrets['password'],
 )
 workouts = tracker.list_all_workouts()
-for i, workout in enumerate(workouts):
-    tracker.workout_saveas_gpx(workout.id, f"{out_dir}/workout{i:.3d}.gpx")
+for workout in workouts:
+    tracker.workout_saveas_gpx(workout['id'], f"{out_dir}/workout_{workout['id']}.gpx")
+    logging.debug(workout['id'])
--- sports_tracker_api/sports_tracker.py
+++ sports_tracker_api/sports_tracker.py
@@ -1,4 +1,6 @@
+import logging
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class SportsTracker:
@@ -6,11 +8,29 @@ class SportsTracker:
     def __init__(self, username: str, password: str):
         self.url = "https://www.sports-tracker.com"
         self.username = username
-        self.driver = webdriver.Safari()
-        self.driver.get(self.url)
+        self.password = password
+        self.driver = None
 
     def list_all_workouts(self):
-        return []
+        self.driver = webdriver.Safari()
+        self.driver.get(f"{self.url}/dashboard")
+        username_textbox = self.driver.find_element_by_class_name("username")
+        username_textbox.send_keys(self.username)
+        password_textbox = self.driver.find_element_by_class_name("password")
+        password_textbox.send_keys(self.password)
+        submit_button = self.driver.find_element_by_class_name("submit")
+        submit_button.click()
+        feeds = self.driver.find_elements_by_class_name("feed-card")
+        logging.info(len(feeds))
+        for feed in feeds:
+            logging.info(feed.text)
+        self.driver.close()
+        return [{'id': "5d8b460e17683adfb9b6240b"}]
 
     def workout_saveas_gpx(self, workout_id: str, output_path: str):
+        self.driver = webdriver.Safari()
+        self.driver.get(f"{self.url}/workout/{self.username}/{workout_id}")
+        edit_button = self.driver.find_elements_by_class_name("workout-edit")
+        logging.info(edit_button)
+        self.driver.close()
         return 0
